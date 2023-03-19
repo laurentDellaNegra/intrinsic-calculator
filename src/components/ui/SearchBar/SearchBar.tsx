efc6d34cd29f6c09eeda9e05972a6176191f68c5
laurentDellaNegra <laurent.dellanegra@gmail.com>
2023-03-19 21:25:09 +0100

    ✨ Plug searchbar

--- src/components/ui/SearchBar/SearchBar.tsx
+++ src/components/ui/SearchBar/SearchBar.tsx
@@ -1,10 +1,17 @@
+import { createSignal } from 'solid-js'
 import LoopIcon from '../../icons/LoopIcon'
 
 export default function SearchBar() {
+  const [text, setText] = createSignal('')
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+    window.location.pathname = `/${text()}`
+  }
   return (
     <div class="flex flex-1 justify-between px-4 sm:px-6 lg:mx-auto lg:max-w-6xl lg:px-8">
       <div class="flex flex-1">
-        <form class="flex w-full md:ml-0" action="#">
+        <form class="flex w-full md:ml-0" onsubmit={handleFormSubmit}>
           <label for="search-field" class="sr-only">
             Search a ticker
           </label>
@@ -21,6 +28,8 @@ export default function SearchBar() {
               class="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
               placeholder="Search a ticker"
               type="search"
+              value={text()}
+              oninput={(e) => setText(e.currentTarget.value)}
             />
           </div>
         </form>

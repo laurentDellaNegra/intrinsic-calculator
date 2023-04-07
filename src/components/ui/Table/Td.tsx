3d4d6f6e1c822b1250fa6ca3a67ff32760e846f2
laurentDellaNegra <laurent.dellanegra@gmail.com>
2023-04-07 15:54:28 +0200

    🚸 Add first sticky column

--- src/components/ui/Table/Td.tsx
+++ src/components/ui/Table/Td.tsx
@@ -5,5 +5,9 @@ interface Props {
 }
 
 export default function Td({ children }: Props) {
-  return <td class="whitespace-nowrap px-3 py-3 text-xs text-gray-900">{children}</td>
+  return (
+    <td class="whitespace-nowrap px-3 py-3 text-xs text-gray-900 first:sticky first:left-0 first:z-10 first:bg-white first:drop-shadow-xl">
+      {children}
+    </td>
+  )
 }
--- src/components/ui/Table/Th.tsx
+++ src/components/ui/Table/Th.tsx
@@ -8,7 +8,7 @@ export default function Th({ children }: Props) {
   return (
     <th
       scope="col"
-      class="whitespace-nowrap bg-gray-50 px-3 py-4 text-left text-xs font-semibold text-gray-900"
+      class="whitespace-nowrap bg-gray-50 px-3 py-4 text-left text-xs font-semibold text-gray-900 first:sticky first:left-0 first:z-10 first:drop-shadow-xl"
     >
       {children}
     </th>
